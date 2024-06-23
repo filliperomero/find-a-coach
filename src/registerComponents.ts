fbbc45316b34480aa5963811fbf1541fe50a72de
filliperomero <fillipe.romero@gmail.com>
2024-06-22 23:44:54 -0600

    Add checkbox component

--- src/registerComponents.ts
+++ src/registerComponents.ts
@@ -2,9 +2,11 @@ import { type App } from 'vue'
 import BaseCard from './components/ui/BaseCard.vue'
 import BaseButton from './components/ui/BaseButton.vue'
 import Badge from './components/ui/Badge/Badge.vue'
+import Checkbox from './components/ui/Checkbox/Checkbox.vue'
 
 export const registerComponents = (app: App) => {
   app.component('BaseCard', BaseCard)
   app.component('BaseButton', BaseButton)
   app.component('Badge', Badge)
+  app.component('Checkbox', Checkbox)
 }
