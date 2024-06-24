ce25c48d54afa04e0daa25618493dd5c25d29ecf
filliperomero <fillipe.romero@gmail.com>
2024-06-24 00:18:45 -0600

    Add coaches filter

---
+++ src/components/coaches/CoachFilter.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+let filters = {
+  frontend: true,
+  backend: true,
+  career: true
+}
+
+const emit = defineEmits({
+  'change-filter': ({ filters }) => {
+    return filters
+  }
+})
+
+function setFilter(event) {
+  const inputId = event.target.id
+  const isActive = event.target.checked
+
+  filters = {
+    ...filters,
+    [inputId]: isActive
+  }
+
+  console.log(filters)
+  emit('change-filter', filters)
+}
+</script>
+
+<template>
+  <BaseCard>
+    <h2>Find Your Coach</h2>
+    <!-- <div class="flex space-x-2 items-center">
+      <Checkbox id="frontend" @change="setFilter" @update:checked="setFilter" />
+      <label for="frontend">Frontend</label>
+    </div> -->
+    <div class="flex flex-col gap-2">
+      <span class="flex items-center gap-2">
+        <input id="frontend" type="checkbox" @change="setFilter" />
+        <label for="frontend">Frontend</label>
+      </span>
+      <!-- <div class="flex space-x-2 items-center">
+        <Checkbox id="backend" @change="setFilter" />
+        <label for="backend">Backend</label>
+      </div> -->
+      <span class="flex items-center gap-2">
+        <input id="backend" type="checkbox" @change="setFilter" />
+        <label for="backend">Backend</label>
+      </span>
+      <!-- <div class="flex space-x-2 items-center">
+        <Checkbox id="career" @change="setFilter" />
+        <label for="career">Career</label>
+      </div> -->
+      <spa class="flex items-center gap-2">
+        <input id="career" type="checkbox" @change="setFilter" />
+        <label for="career">Career</label>
+      </spa>
+    </div>
+  </BaseCard>
+</template>
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,12 +1,38 @@
 <script setup lang="ts">
 import CoachItem from '@/components/coaches/CoachItem.vue'
+import CoachFilter from '@/components/coaches/CoachFilter.vue'
 import { useCoachesStore } from '@/store'
+import { computed, reactive } from 'vue'
 
 const store = useCoachesStore()
+let activeFilters = reactive({
+  frontend: false,
+  backend: false,
+  career: false
+})
+
+function setFilters(filters) {
+  Object.assign(activeFilters, filters)
+}
+
+const filteredCoaches = computed(() => {
+  const coaches = store.getCoaches
+  const filteredCoaches = coaches.filter((coach) => {
+    if (activeFilters.frontend && coach.areas.includes('frontend')) return true
+    else if (activeFilters.backend && coach.areas.includes('backend')) return true
+    else if (activeFilters.career && coach.areas.includes('career')) return true
+
+    return false
+  })
+
+  return filteredCoaches
+})
 </script>
 
 <template>
-  <section>FILTER</section>
+  <section>
+    <CoachFilter @change-filter="setFilters" />
+  </section>
 
   <section>
     <BaseCard>
@@ -15,10 +41,10 @@ const store = useCoachesStore()
         <BaseButton link to="/register">Register as a Coach</BaseButton>
       </div>
       <ul v-if="store.hasCoaches">
-        <CoachItem 
-          v-for="coach in store.getCoaches" 
-          :key="coach.id" 
-          :id="coach.id" 
+        <CoachItem
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
           :first-name="coach.firstName"
           :last-name="coach.lastName"
           :rate="coach.hourlyRate"
